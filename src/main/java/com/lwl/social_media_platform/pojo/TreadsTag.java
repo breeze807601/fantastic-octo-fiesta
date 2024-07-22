b5313da38b3e014735da7741939dd11789e657e7
YINOQI <1028@yinoqi.com>
2024-07-22 22:43:20 +0800

    将属性 tread 修改 为 treads

--- src/main/java/com/lwl/social_media_platform/pojo/TreadsTag.java
+++ src/main/java/com/lwl/social_media_platform/pojo/TreadsTag.java
@@ -7,6 +7,6 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class TreadsTag {
     private Long id;
-    private Long treadId;
+    private Long treadsId;
     private Long tagId;
 }
