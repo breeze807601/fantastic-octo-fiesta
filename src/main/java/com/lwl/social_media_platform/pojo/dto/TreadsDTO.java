bcaf8f6b0bbd751443aab374c7b68bf3f2dc790c
YINOQI <1028@yinoqi.com>
2024-07-23 14:45:46 +0800

    动态相关：添加图片相关功能

--- src/main/java/com/lwl/social_media_platform/pojo/dto/TreadsDTO.java
+++ src/main/java/com/lwl/social_media_platform/pojo/dto/TreadsDTO.java
@@ -1,5 +1,6 @@
 package com.lwl.social_media_platform.pojo.dto;
 
+import com.lwl.social_media_platform.pojo.Image;
 import com.lwl.social_media_platform.pojo.Treads;
 import com.lwl.social_media_platform.pojo.TreadsTag;
 import lombok.Data;
@@ -9,4 +10,5 @@ import java.util.List;
 @Data
 public class TreadsDTO extends Treads {
     private List<TreadsTag> treadsTagList;
+    private List<Image> imageList;
 }
