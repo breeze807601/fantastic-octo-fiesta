98689454db2524625ec66ca570e118176383881e
YINOQI <1028@yinoqi.com>
2024-07-24 17:50:50 +0800

    添加获取动态列表方法

--- src/main/java/com/lwl/social_media_platform/service/TreadsService.java
+++ src/main/java/com/lwl/social_media_platform/service/TreadsService.java
@@ -6,6 +6,8 @@ import com.lwl.social_media_platform.domain.pojo.Treads;
 import com.lwl.social_media_platform.domain.dto.TreadsDTO;
 import com.lwl.social_media_platform.domain.vo.TreadsVo;
 
+import java.util.List;
+
 public interface TreadsService extends IService<Treads> {
     Result<String> publish(TreadsDTO treadsDTO);
 
@@ -13,6 +15,9 @@ public interface TreadsService extends IService<Treads> {
 
     Result<TreadsVo> getTread(Long id);
 
+    Result<List<TreadsVo>> getTreadsList(Long userId);
+
     Result<String> updateTread(TreadsDTO treadsDTO);
+
 
 }
