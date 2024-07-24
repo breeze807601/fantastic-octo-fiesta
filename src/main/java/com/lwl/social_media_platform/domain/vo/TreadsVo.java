cbcea0363dede576aa59be38202ac534164703d2
YINOQI <1028@yinoqi.com>
2024-07-24 17:50:33 +0800

    添加点赞数

--- src/main/java/com/lwl/social_media_platform/domain/vo/TreadsVo.java
+++ src/main/java/com/lwl/social_media_platform/domain/vo/TreadsVo.java
@@ -4,12 +4,18 @@ import com.lwl.social_media_platform.domain.pojo.Image;
 import com.lwl.social_media_platform.domain.pojo.Tag;
 import com.lwl.social_media_platform.domain.pojo.Treads;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 
+
 @Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
 public class TreadsVo extends Treads {
     private List<Tag> tagList;
     private List<Image>  imageList;
+    private Long supportNum;
     private Boolean isFollow;
 }
