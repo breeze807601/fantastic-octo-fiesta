b51cde67e2dc68c523ea8c183fcd628e72707bd9
YINOQI <1028@yinoqi.com>
2024-08-12 16:17:24 +0800

    添加 获取最近高点赞数动态的id及其点赞数的方法

--- src/main/java/com/lwl/social_media_platform/service/SupportService.java
+++ src/main/java/com/lwl/social_media_platform/service/SupportService.java
@@ -3,6 +3,11 @@ package com.lwl.social_media_platform.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.lwl.social_media_platform.domain.pojo.Support;
 
+import java.util.List;
+import java.util.Map;
+
 public interface SupportService extends IService<Support> {
+
+    Map<Long, Long> getCurrentMaxSupportNum(List<Long> treadIds);
 
 }
