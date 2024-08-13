a1d509bc35177588ab379a0fbc431d2df3641410
YINOQI <1028@yinoqi.com>
2024-08-13 16:45:54 +0800

    添加redis相关逻辑

--- src/main/java/com/lwl/social_media_platform/service/impl/UserServiceImpl.java
+++ src/main/java/com/lwl/social_media_platform/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.lwl.social_media_platform.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lwl.social_media_platform.domain.pojo.User;
 import com.lwl.social_media_platform.domain.vo.UserVo;
@@ -7,22 +9,42 @@ import com.lwl.social_media_platform.mapper.UserMapper;
 import com.lwl.social_media_platform.service.UserService;
 import com.lwl.social_media_platform.utils.BeanUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.lwl.social_media_platform.utils.RedisContent.USER_KEY;
+import static com.lwl.social_media_platform.utils.RedisContent.USER_TTL;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
     @Override
     public UserVo getUserById(Long id) {
+        String userVoStr = stringRedisTemplate.opsForValue().get(USER_KEY + id);
 
-        User user = this.getById(id);
+        if (StrUtil.isNotBlank(userVoStr)) {
+            return JSONUtil.toBean(userVoStr, UserVo.class);
+        } else {
 
-        return BeanUtils.copyProperties(user, UserVo.class);
+            User user = this.getById(id);
+
+            UserVo userVo = BeanUtils.copyProperties(user, UserVo.class);
+            stringRedisTemplate.opsForValue().set(USER_KEY + id, JSONUtil.toJsonStr(userVo), USER_TTL, TimeUnit.DAYS);
+            return userVo;
+        }
     }
 
     @Override
     public UserVo updateUser(User user) {
         this.updateById(user);
-        return BeanUtils.copyProperties(user, UserVo.class);
+
+        UserVo userVo = BeanUtils.copyProperties(user, UserVo.class);
+        stringRedisTemplate.opsForValue().set(USER_KEY + user.getId(), JSONUtil.toJsonStr(userVo), USER_TTL, TimeUnit.DAYS);
+        return userVo;
     }
 }
---
+++ src/main/java/com/lwl/social_media_platform/utils/RedisContent.java
@@ -0,0 +1,6 @@
+package com.lwl.social_media_platform.utils;
+
+public class RedisContent {
+    public static final String USER_KEY = "user:";
+    public static final Long USER_TTL = 3L;
+}
