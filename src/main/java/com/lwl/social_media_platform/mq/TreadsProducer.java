fd646fcb4447bfa5c245147d8b5c59c709df93a0
YINOQI <1028@yinoqi.com>
2024-08-19 23:59:31 +0800

    消息队列初始化

---
+++ src/main/java/com/lwl/social_media_platform/mq/TreadsProducer.java
@@ -0,0 +1,45 @@
+package com.lwl.social_media_platform.mq;
+
+import com.alibaba.fastjson.JSON;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.MessageConst;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TreadsProducer {
+
+    private final RocketMQTemplate rocketMQTemplate;
+
+    @Value("${rocketmq.producer.topic}")
+    private String topic;
+
+    public void sendMessage(String treadsJSON) {
+        String keys = UUID.randomUUID().toString();
+        Map<String, String> keyMap = new HashMap<>();
+        keyMap.put("keys", keys);
+        keyMap.put("treadsJSON",treadsJSON);
+        Message<Map<String, String>> build = MessageBuilder
+                .withPayload(keyMap)
+                .setHeader(MessageConst.PROPERTY_KEYS, keys)
+                .build();
+        SendResult sendResult;
+        try {
+            sendResult = rocketMQTemplate.syncSend(topic, build, 2000L);
+            log.info("动态发送结果：{}，消息ID：{}，消息Keys：{}", sendResult.getSendStatus(), sendResult.getMsgId(), keys);
+        } catch (Throwable ex) {
+            log.error("[消息访问统计监控] 消息发送失败，消息体：{}", JSON.toJSONString(keyMap), ex);
+        }
+    }
+}
