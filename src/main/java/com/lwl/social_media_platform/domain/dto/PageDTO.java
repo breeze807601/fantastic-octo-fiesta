48bebdb6f5ad1b51e26aa4d3d8ad1bfe0089437e
YINOQI <1028@yinoqi.com>
2024-08-23 22:09:03 +0800

    添加链式编程

--- src/main/java/com/lwl/social_media_platform/domain/dto/PageDTO.java
+++ src/main/java/com/lwl/social_media_platform/domain/dto/PageDTO.java
@@ -4,12 +4,14 @@ package com.lwl.social_media_platform.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Accessors(chain = true)
 public class PageDTO<T> {
     protected Long total;
     protected Long pages;
